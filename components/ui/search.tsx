642dd4060cdfa999f2bac77b7d6c98cc573b487e
Nick Harbaugh <nick@periscopic.com>
2024-03-08 11:35:25 -0600

    Add headless ui combobox and fuse.js for search

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,15 +1,221 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import iconSearch from '@/public/images/icon-search.svg'
 import cn from 'classnames'
+import Fuse from 'fuse.js'
+import { Combobox } from '@headlessui/react'
 
 type Props = {
   placeholder: string
   size?: string
 }
 
+const books = [
+  {
+    "title": "Old Man's War",
+    "author": {
+      "firstName": "John",
+      "lastName": "Scalzi"
+    }
+  },
+  {
+    "title": "The Lock Artist",
+    "author": {
+      "firstName": "Steve",
+      "lastName": "Hamilton"
+    }
+  },
+  {
+    "title": "HTML5",
+    "author": {
+      "firstName": "Remy",
+      "lastName": "Sharp"
+    }
+  },
+  {
+    "title": "Right Ho Jeeves",
+    "author": {
+      "firstName": "P.D",
+      "lastName": "Woodhouse"
+    }
+  },
+  {
+    "title": "The Code of the Wooster",
+    "author": {
+      "firstName": "P.D",
+      "lastName": "Woodhouse"
+    }
+  },
+  {
+    "title": "Thank You Jeeves",
+    "author": {
+      "firstName": "P.D",
+      "lastName": "Woodhouse"
+    }
+  },
+  {
+    "title": "The DaVinci Code",
+    "author": {
+      "firstName": "Dan",
+      "lastName": "Brown"
+    }
+  },
+  {
+    "title": "Angels & Demons",
+    "author": {
+      "firstName": "Dan",
+      "lastName": "Brown"
+    }
+  },
+  {
+    "title": "The Silmarillion",
+    "author": {
+      "firstName": "J.R.R",
+      "lastName": "Tolkien"
+    }
+  },
+  {
+    "title": "Syrup",
+    "author": {
+      "firstName": "Max",
+      "lastName": "Barry"
+    }
+  },
+  {
+    "title": "The Lost Symbol",
+    "author": {
+      "firstName": "Dan",
+      "lastName": "Brown"
+    }
+  },
+  {
+    "title": "The Book of Lies",
+    "author": {
+      "firstName": "Brad",
+      "lastName": "Meltzer"
+    }
+  },
+  {
+    "title": "Lamb",
+    "author": {
+      "firstName": "Christopher",
+      "lastName": "Moore"
+    }
+  },
+  {
+    "title": "Fool",
+    "author": {
+      "firstName": "Christopher",
+      "lastName": "Moore"
+    }
+  },
+  {
+    "title": "Incompetence",
+    "author": {
+      "firstName": "Rob",
+      "lastName": "Grant"
+    }
+  },
+  {
+    "title": "Fat",
+    "author": {
+      "firstName": "Rob",
+      "lastName": "Grant"
+    }
+  },
+  {
+    "title": "Colony",
+    "author": {
+      "firstName": "Rob",
+      "lastName": "Grant"
+    }
+  },
+  {
+    "title": "Backwards, Red Dwarf",
+    "author": {
+      "firstName": "Rob",
+      "lastName": "Grant"
+    }
+  },
+  {
+    "title": "The Grand Design",
+    "author": {
+      "firstName": "Stephen",
+      "lastName": "Hawking"
+    }
+  },
+  {
+    "title": "The Book of Samson",
+    "author": {
+      "firstName": "David",
+      "lastName": "Maine"
+    }
+  },
+  {
+    "title": "The Preservationist",
+    "author": {
+      "firstName": "David",
+      "lastName": "Maine"
+    }
+  },
+  {
+    "title": "Fallen",
+    "author": {
+      "firstName": "David",
+      "lastName": "Maine"
+    }
+  },
+  {
+    "title": "Monster 1959",
+    "author": {
+      "firstName": "David",
+      "lastName": "Maine"
+    }
+  }
+]
+
+const fuseOptions = {
+  // isCaseSensitive: false,
+  // includeScore: false,
+  // shouldSort: true,
+  // includeMatches: false,
+  // findAllMatches: false,
+  // minMatchCharLength: 1,
+  // location: 0,
+  // threshold: 0.6,
+  // distance: 100,
+  // useExtendedSearch: false,
+  // ignoreLocation: false,
+  // ignoreFieldNorm: false,
+  // fieldNormWeight: 1,
+  keys: [
+    "title",
+    "author.firstName"
+  ]
+}
+
 const Search = ({ placeholder, size }: Props) => {
+  const [selectedBook, setSelectedBook] = useState(books[0])
+  const [query, setQuery] = useState('')
+
+  const fuse = new Fuse(books, fuseOptions)
+
+  const filteredBooks = fuse.search(query)
+
   return (
     <div id='search' className='w-full bg-grey-100 rounded-sm py-3 px-5'>
+      {/* <Combobox value={selectedBook} onChange={setSelectedBook}>
+        <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+        <Combobox.Options>
+          {filteredBooks.map((book: any) => (
+            <Combobox.Option key={book.item.title} value={book.item.title}>
+              {book.item.title}
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
+      </Combobox> */}
       <div id='search-input' className='flex items-center'>
         <Image
           src={iconSearch}
