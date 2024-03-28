6c9e78086c0ccb342f7e54eed1e3e96bd855a77f
Nick Harbaugh <nick@periscopic.com>
2024-03-28 13:47:25 -0500

    Search placeholder and hero link changes

--- components/home/home-hero.tsx
+++ components/home/home-hero.tsx
@@ -5,10 +5,6 @@ import Image from 'next/image'
 import { Transition } from '@headlessui/react';
 import Search from '@/components/ui/search'
 import atlasLogo from '@/public/images/atlas-logo.svg'
-import iconBars from '@/public/images/icon-bars.svg'
-import iconBadge from '@/public/images/icon-badge.svg'
-import iconPage from '@/public/images/icon-page.svg'
-import iconArrow from '@/public/images/icon-arrow-right.svg'
 import { Home, SearchItem } from '@/types/sanity.types'
 import cn from 'classnames'
 
@@ -19,33 +15,6 @@ type Props = {
 
 const HomeHero = ({ searchContent, content }: Props) => {
   const [searchBoxActive, setSearchBoxActive] = useState(false)
-
-  const links = [
-    {
-      id: 'home-splash-links-0',
-      target: '#spotlight-grid',
-      text: 'Explore our data',
-      width: 'max-w-[100px]',
-      image: iconBars,
-      alt: 'Bars icon',
-    },
-    {
-      id: 'home-splash-links-1',
-      target: '#insight-carousel',
-      text: 'Read about our data from experts',
-      width: 'max-w-[200px]',
-      image: iconBadge,
-      alt: 'Badge icon',
-    },
-    {
-      id: 'home-splash-links-2',
-      target: '#impact-carousel',
-      text: 'Read about our impact',
-      width: 'max-w-[150px]',
-      image: iconPage,
-      alt: 'Page icon',
-    },
-  ]
 
   const handleLinkClick = (e: any, target: any) => {
     e.preventDefault(); // Prevent default anchor behavior
@@ -80,35 +49,26 @@ const HomeHero = ({ searchContent, content }: Props) => {
             { '-translate-y-[220px]': searchBoxActive }
           )}>
             <p className='text-grey-600 text-lg font-medium mb-3'>Know what you’re looking for?</p>
-            <Search data={searchContent} placeholder='Search for evidence-based research and data...' searchBoxActive={searchBoxActive} setSearchBoxActive={setSearchBoxActive} />
+            <Search data={searchContent} placeholder='Search for evidence-based research and data by keyword or phrase...' searchBoxActive={searchBoxActive} setSearchBoxActive={setSearchBoxActive} />
           </div>
         </div>
       </div>
 
       {/* <div id='intro-nav' className='w-1/4 h-[calc(100vh-40px)] flex flex-col gap-5'> */}
       <div id='intro-nav' className='w-1/4 h-full flex flex-col gap-5'>
-        {links.map((link) =>
           <a
-            key={link.id}
-            href={link.target}
-            onClick={(e) => handleLinkClick(e, link.target)}
-            className='relative w-full h-1/3 transition-colors bg-brand-green hover:bg-brand-dark-green'
+            href='#spotlight-grid'
+            onClick={(e) => handleLinkClick(e, '#spotlight-grid')}
+            className='flex items-center gap-3 px-10 relative w-full h-full transition-colors bg-brand-green hover:bg-brand-dark-green'
           >
-            <Image // link icon
-              src={link.image}
-              alt={link.alt}
-              width={20}
-              className='absolute top-6 left-6'
-            />
-            <span className={`absolute ${link.width} bottom-6 left-6 text-white text-xl text-medium uppercase`}>{link.text}</span>
+            <span className='text-white text-xl text-medium uppercase'>Interact with data</span>
             <Image // arrow icon
-              src={iconArrow}
+              src={'/images/icon-arrow-right.svg'}
               alt='Right arrow icon'
               width={20}
-              className='absolute bottom-6 right-6'
+              height={20}
             />
           </a>
-        )}
       </div>
     </div>
   )
