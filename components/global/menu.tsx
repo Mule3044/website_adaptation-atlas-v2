55e8e56fdcac3883e7c3ba05a2b1760b05e98a68
Alex <alexander.barbati@gmail.com>
2024-06-18 02:15:09 -0400

    test commit to push build

--- components/global/menu.tsx
+++ components/global/menu.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import cn from 'classnames'
-import Share from '@/components/ui/share'
-import { BiX, BiMenu, BiChevronLeft } from 'react-icons/bi'
-import { Spotlight, Insight, Impact, Settings } from '@/types/sanity.types'
+import React, { useState } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import cn from "classnames"
+import Share from "@/components/ui/share"
+import { BiX, BiMenu, BiChevronLeft } from "react-icons/bi"
+import { Spotlight, Insight, Impact, Settings } from "@/types/sanity.types"
 
 type Props = {
   spotlights: Spotlight[]
@@ -15,31 +15,43 @@ type Props = {
   setMenuActive: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenuActive }: Props) => {
-  const filteredSpotlights = spotlights.filter(item => !item.comingSoon)
-  const [menuLevelActive, setMenuLevelActive] = useState<'primary' | 'secondary' | 'tertiary'>('primary')
-  const [tertiaryMenuContent, setTertiaryMenuContent] = useState<{
-    id: string
-    slug: string
-    title: string
-    posts: Spotlight[] | Insight[] | Impact[]
-  } | undefined>()
+const Menu = ({
+  spotlights,
+  insights,
+  impacts,
+  siteSettings,
+  menuActive,
+  setMenuActive,
+}: Props) => {
+  const filteredSpotlights = spotlights.filter((item) => !item.comingSoon)
+  const [menuLevelActive, setMenuLevelActive] = useState<
+    "primary" | "secondary" | "tertiary"
+  >("primary")
+  const [tertiaryMenuContent, setTertiaryMenuContent] = useState<
+    | {
+        id: string
+        slug: string
+        title: string
+        posts: Spotlight[] | Insight[] | Impact[]
+      }
+    | undefined
+  >()
   const secondaryMenuLinks = [
     {
-      id: 'spotlights',
-      slug: 'data-spotlights',
+      id: "spotlights",
+      slug: "data-spotlights",
       title: siteSettings.contentTypes.spotlightsTitle,
       posts: filteredSpotlights,
     },
     {
-      id: 'insights',
-      slug: 'data-insights',
+      id: "insights",
+      slug: "data-insights",
       title: siteSettings.contentTypes.insightsTitle,
       posts: insights,
     },
     {
-      id: 'impacts',
-      slug: 'data-in-practice',
+      id: "impacts",
+      slug: "data-in-practice",
       title: siteSettings.contentTypes.impactsTitle,
       posts: impacts,
     },
@@ -48,73 +60,87 @@ const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenu
   const handleToggleMenu = () => {
     setMenuActive(!menuActive)
     // Reset nested menus on main menu toggle
-    setMenuLevelActive('primary')
+    setMenuLevelActive("primary")
   }
 
   const activatePrimaryMenu = () => {
-    setMenuLevelActive('primary')
+    setMenuLevelActive("primary")
   }
 
   const activateSecondaryMenu = () => {
-    setMenuLevelActive('secondary')
+    setMenuLevelActive("secondary")
   }
 
   const activateTertiaryMenu = (menuType: any) => {
-    setMenuLevelActive('tertiary')
-    const menuContent = secondaryMenuLinks.find(item => item.id === menuType)
+    setMenuLevelActive("tertiary")
+    const menuContent = secondaryMenuLinks.find((item) => item.id === menuType)
     setTertiaryMenuContent(menuContent)
   }
 
   const menuLinks = [
-    { id: 'our-work', link: '/', title: siteSettings.menu.workTitle },
-    { id: 'about', link: `/${siteSettings.menu.aboutSlug}`, title: siteSettings.menu.aboutTitle },
-    { id: 'get-involved', link: `/${siteSettings.menu.getInvolvedSlug}`, title: siteSettings.menu.getInvolvedTitle },
+    { id: "our-work", link: "/", title: siteSettings.menu.workTitle },
+    {
+      id: "about",
+      link: `/${siteSettings.menu.aboutSlug}`,
+      title: siteSettings.menu.aboutTitle,
+    },
+    {
+      id: "get-involved",
+      link: `/${siteSettings.menu.getInvolvedSlug}`,
+      title: siteSettings.menu.getInvolvedTitle,
+    },
   ]
 
   const year = new Date().getFullYear()
 
   return (
-    <nav className='fixed z-50'>
+    <nav className="fixed z-50">
       <button
-        className={
-          cn(
-            'fixed top-5 left-5 bg-brand-green text-white p-2 rounded-full cursor-pointer',
-            'hover:bg-grey-100 hover:text-brand-green transition-colors duration-300 ease-in-out',
-          )
-        }
+        className={cn(
+          "fixed top-5 left-5 bg-brand-green text-white p-2 rounded-full cursor-pointer",
+          "hover:bg-grey-100 hover:text-brand-green transition-colors duration-300 ease-in-out"
+        )}
         onClick={handleToggleMenu}
       >
-        <BiMenu className='h-6 w-6' />
+        <BiMenu className="h-6 w-6" />
       </button>
 
       {/* Menu container */}
       <div
         className={cn(
-          menuActive ? 'opacity-100 pointer-events-default' : 'opacity-0 pointer-events-none',
-          'fixed z-50 top-0 right-0 bottom-0 left-0 bg-brand-green transition duration-400',
+          menuActive
+            ? "opacity-100 pointer-events-default"
+            : "opacity-0 pointer-events-none",
+          "fixed z-50 top-0 right-0 bottom-0 left-0 bg-brand-green transition duration-400"
         )}
       >
         {/* <div className='flex items-center h-3/4 md:h-full md:w-3/4 absolute top-0 ml-[45px] md:ml-[300px]'> */}
-        <div className='relative flex items-center h-3/4 lg:h-full top-0'>
+        <div className="relative flex items-center h-3/4 lg:h-full top-0">
           {/* Primary menu */}
-          <div className={cn(
-            'absolute flex flex-col gap-5 md:gap-7 lg:gap-14',
-            'transition-all duration-300 ease-in-out transform',
-            'pl-[60px] pr-[20px] lg:pl-[300px] my-[100px]',
-            {
-              'opacity-100 translate-x-0': menuLevelActive === 'primary', // Active state classes
-              '-translate-x-[100%] opacity-0': menuLevelActive !== 'primary', // Inactive state classes
-              'pointer-events-auto': menuActive && menuLevelActive === 'primary', // Pointer events when active
-              'pointer-events-none': !(menuActive && menuLevelActive === 'primary') // Pointer events when not active
-            }
-          )}>
+          <div
+            className={cn(
+              "absolute flex flex-col gap-5 md:gap-7 lg:gap-14",
+              "transition-all duration-300 ease-in-out transform",
+              "pl-[60px] pr-[20px] lg:pl-[300px] my-[100px]",
+              {
+                "opacity-100 translate-x-0": menuLevelActive === "primary", // Active state classes
+                "-translate-x-[100%] opacity-0": menuLevelActive !== "primary", // Inactive state classes
+                "pointer-events-auto":
+                  menuActive && menuLevelActive === "primary", // Pointer events when active
+                "pointer-events-none": !(
+                  menuActive && menuLevelActive === "primary"
+                ), // Pointer events when not active
+              }
+            )}
+          >
             {menuLinks.map((item) => (
-              <div key={item.id} className='text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide'>
+              <div
+                key={item.id}
+                className="text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide"
+              >
                 {/* Handle click for Our Work to toggle the nested menu */}
-                {item.id === 'our-work' ? (
-                  <button onClick={activateSecondaryMenu}>
-                    {item.title}
-                  </button>
+                {item.id === "our-work" ? (
+                  <button onClick={activateSecondaryMenu}>{item.title}</button>
                 ) : (
                   <Link href={item.link} onClick={handleToggleMenu}>
                     {item.title}
@@ -125,32 +151,39 @@ const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenu
           </div>
 
           {/* Secondary menu */}
-          <div className={cn(
-            'absolute flex flex-col gap-5 md:gap-7 lg:gap-14',
-            'transition-all duration-300 ease-in-out transform',
-            'pl-[60px] pr-[20px] lg:pl-[300px] my-[100px]',
-            {
-              'opacity-100 translate-x-0': menuLevelActive === 'secondary', // Active state classes
-              'translate-x-[100%] opacity-0': menuLevelActive === 'primary', // Inactive state classes
-              '-translate-x-[100%] opacity-0': menuLevelActive === 'tertiary', // Inactive state classes
-              'pointer-events-auto': menuActive && menuLevelActive === 'secondary', // Pointer events when active
-              'pointer-events-none': !(menuActive && menuLevelActive === 'secondary') // Pointer events when not active
-            }
-          )}>
-            <div className='relative'>
-              <h2 className='text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide opacity-70'>{siteSettings.menu.workTitle}</h2>
+          <div
+            className={cn(
+              "absolute flex flex-col gap-5 md:gap-7 lg:gap-14",
+              "transition-all duration-300 ease-in-out transform",
+              "pl-[60px] pr-[20px] lg:pl-[300px] my-[100px]",
+              {
+                "opacity-100 translate-x-0": menuLevelActive === "secondary", // Active state classes
+                "translate-x-[100%] opacity-0": menuLevelActive === "primary", // Inactive state classes
+                "-translate-x-[100%] opacity-0": menuLevelActive === "tertiary", // Inactive state classes
+                "pointer-events-auto":
+                  menuActive && menuLevelActive === "secondary", // Pointer events when active
+                "pointer-events-none": !(
+                  menuActive && menuLevelActive === "secondary"
+                ), // Pointer events when not active
+              }
+            )}
+          >
+            <div className="relative">
+              <h2 className="text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide opacity-70">
+                {siteSettings.menu.workTitle}
+              </h2>
               <button
-                className='absolute -left-12 -top-0.5 lg:top-1 bg-white rounded-full transition-opacity opacity-70 hover:opacity-100'
+                className="absolute -left-12 -top-0.5 lg:top-1 bg-white rounded-full transition-opacity opacity-70 hover:opacity-100"
                 onClick={activatePrimaryMenu}
               >
-                <BiChevronLeft className='text-brand-green h-8 w-8' />
+                <BiChevronLeft className="text-brand-green h-8 w-8" />
               </button>
             </div>
 
             {secondaryMenuLinks.map((item) => (
               <button
                 key={item.id}
-                className='text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide text-left'
+                className="text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide text-left"
                 onClick={() => activateTertiaryMenu(item.id)}
               >
                 {item.title}
@@ -159,25 +192,32 @@ const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenu
           </div>
 
           {/* Tertiary menu */}
-          <div className={cn(
-            'absolute flex flex-col gap-5 md:gap-7 lg:gap-14 menu-tertiary',
-            'pt-[100px] pb-[200px] h-full w-full overflow-y-auto overflow-x-hidden',
-            'pl-[60px] pr-[20px] lg:pl-[300px] md:pr-[50px] lg:pr-[150px] xl:pr-[250px] my-[100px]',
-            'transition-all duration-300 ease-in-out transform',
-            {
-              'opacity-100 translate-x-0': menuLevelActive === 'tertiary', // Active state classes
-              'translate-x-[100%] opacity-0': menuLevelActive !== 'tertiary', // Inactive state classes
-              'pointer-events-auto': menuActive && menuLevelActive === 'tertiary', // Pointer events when active
-              'pointer-events-none': !(menuActive && menuLevelActive === 'tertiary') // Pointer events when not active
-            }
-          )}>
-            <div className='relative lg:mt-[160px]'>
-              <h2 className='text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide opacity-70'>{tertiaryMenuContent?.title}</h2>
+          <div
+            className={cn(
+              "absolute flex flex-col gap-5 md:gap-7 lg:gap-14 menu-tertiary",
+              "pt-[100px] pb-[200px] h-full w-full overflow-y-auto overflow-x-hidden",
+              "pl-[60px] pr-[20px] lg:pl-[300px] md:pr-[50px] lg:pr-[150px] xl:pr-[250px] my-[100px]",
+              "transition-all duration-300 ease-in-out transform",
+              {
+                "opacity-100 translate-x-0": menuLevelActive === "tertiary", // Active state classes
+                "translate-x-[100%] opacity-0": menuLevelActive !== "tertiary", // Inactive state classes
+                "pointer-events-auto":
+                  menuActive && menuLevelActive === "tertiary", // Pointer events when active
+                "pointer-events-none": !(
+                  menuActive && menuLevelActive === "tertiary"
+                ), // Pointer events when not active
+              }
+            )}
+          >
+            <div className="relative lg:mt-[160px]">
+              <h2 className="text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide opacity-70">
+                {tertiaryMenuContent?.title}
+              </h2>
               <button
-                className='absolute -left-12 -top-0.5 lg:top-1 bg-white rounded-full transition-opacity opacity-70 hover:opacity-100'
+                className="absolute -left-12 -top-0.5 lg:top-1 bg-white rounded-full transition-opacity opacity-70 hover:opacity-100"
                 onClick={activateSecondaryMenu}
               >
-                <BiChevronLeft className='text-brand-green h-8 w-8' />
+                <BiChevronLeft className="text-brand-green h-8 w-8" />
               </button>
             </div>
 
@@ -185,19 +225,21 @@ const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenu
               <Link
                 key={item.slug}
                 href={`/${tertiaryMenuContent?.slug}/${item.slug}`}
-                className='text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide'
+                className="text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide"
                 onClick={handleToggleMenu}
               >
                 {item.title}
               </Link>
             ))}
           </div>
-
         </div>
 
         {/* Menu header */}
-        <div id='menu-logo' className='fixed top-0 z-50 flex items-center justify-center w-full pt-6'>
-          <Link href='/' onClick={handleToggleMenu}>
+        <div
+          id="menu-logo"
+          className="fixed top-0 z-50 flex items-center justify-center w-full pt-6"
+        >
+          <Link href="/" onClick={handleToggleMenu}>
             <Image
               src={siteSettings.logoLight}
               alt={siteSettings.logoLightAlt}
@@ -207,21 +249,38 @@ const Menu = ({ spotlights, insights, impacts, siteSettings, menuActive, setMenu
           </Link>
         </div>
         <button
-          className='absolute z-50 top-5 left-5 hover:opacity-100 opacity-90 transition-opacity duration-300 ease-in-out'
+          className="absolute z-50 top-5 left-5 hover:opacity-100 opacity-90 transition-opacity duration-300 ease-in-out"
           onClick={handleToggleMenu}
         >
-          <BiX className='text-white h-10 w-10' />
+          <BiX className="text-white h-10 w-10" />
         </button>
 
         {/* Menu footer */}
-        <div className='fixed right-0 bottom-0 left-0 z-50'>
-          <footer className='flex flex-col lg:flex-row justify-between w-full p-5 text-white'>
-            <p className='order-2 lg:order-1'>© {year} {siteSettings.footer.copyright}</p>
-            <div className='relative flex flex-col lg:flex-row order-1 lg:order-2 gap-0 md:gap-4 mb-1 md:mb-5'>
-              <a href={siteSettings.footer.mailingListLink} className='font-medium text-white opacity-90 hover:opacity-100 transition-opacity'>{siteSettings.footer.mailingListLabel}</a>
-              <a href={siteSettings.footer.feedbackLink} className='font-medium text-white opacity-90 hover:opacity-100 transition-opacity'>{siteSettings.footer.feedbackLabel}</a>
-              <a href={siteSettings.footer.contactLink} className='font-medium text-white opacity-90 hover:opacity-100 transition-opacity'>{siteSettings.footer.contactLabel}</a>
-              <Share type='light' />
+        <div className="fixed right-0 bottom-0 left-0 z-50">
+          <footer className="flex flex-col lg:flex-row justify-between w-full p-5 text-white">
+            <p className="order-2 lg:order-1">
+              © {year} {siteSettings.footer.copyright}
+            </p>
+            <div className="relative flex flex-col lg:flex-row order-1 lg:order-2 gap-0 md:gap-4 mb-1 md:mb-5">
+              <a
+                href={siteSettings.footer.mailingListLink}
+                className="font-medium text-white opacity-90 hover:opacity-100 transition-opacity"
+              >
+                {siteSettings.footer.mailingListLabel}
+              </a>
+              <a
+                href={siteSettings.footer.feedbackLink}
+                className="font-medium text-white opacity-90 hover:opacity-100 transition-opacity"
+              >
+                {siteSettings.footer.feedbackLabel}
+              </a>
+              <a
+                href={siteSettings.footer.contactLink}
+                className="font-medium text-white opacity-90 hover:opacity-100 transition-opacity"
+              >
+                {siteSettings.footer.contactLabel}
+              </a>
+              <Share type="light" />
             </div>
           </footer>
         </div>
