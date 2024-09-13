6cfd472fbe7424a11a5d42190dda9948100d714c
Alexander Jon Barbati <alexander.barbati@gmail.com>
2024-09-12 20:56:45 -0400

    test deployment

--- components/ui/search-modal.tsx
+++ components/ui/search-modal.tsx
@@ -8,71 +8,111 @@ import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { breakpoints } from "@/lib/constants"
 import { usePathname, useSearchParams } from "next/navigation"
 
-const SearchModal = ({modalState}: any) => {
-    const [open, setOpen] = React.useState(false)
-    const closeAll = () => {
-        setOpen(false)
+const SearchModal = ({ modalState }: any) => {
+  const [open, setOpen] = React.useState(false)
+  const closeAll = () => {
+    setOpen(false)
+  }
+  const isDesktop = useMediaQuery(breakpoints.lg)
+  const isLgScreen = useMediaQuery(breakpoints.xl)
+  const isTablet = useMediaQuery(breakpoints.md)
+  const isPhone = useMediaQuery(breakpoints.sm)
+  const { homeContent, searchContent } = useSanityData()
+  const ref = useRef<HTMLDivElement>(null)
+  const [translateY, setTranslateY] = useState(0)
+  const [searchBoxActive, setSearchBoxActive] = useState(false)
+  const searchPlaceholder = isLgScreen
+    ? homeContent?.search.placeholder
+    : homeContent?.search.placeholderShort
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const setModalOpen = () => {
+    setOpen(true)
+  }
+  useEffect(() => {
+    setOpen(false)
+  }, [pathname, searchParams])
+  useEffect(() => {
+    modalState(open)
+  }, [open])
+  useEffect(() => {
+    if (open) {
+      setOpen(false)
     }
-    const isDesktop = useMediaQuery(breakpoints.lg);
-    const isLgScreen = useMediaQuery(breakpoints.xl)
-    const isTablet = useMediaQuery(breakpoints.md);
-    const isPhone = useMediaQuery(breakpoints.sm);
-        const {
-        homeContent,
-        searchContent
-    } = useSanityData()
-    const ref = useRef<HTMLDivElement>(null)
-    const [translateY, setTranslateY] = useState(0)
-    const [searchBoxActive, setSearchBoxActive] = useState(false);
-    const searchPlaceholder = isLgScreen ? homeContent?.search.placeholder : homeContent?.search.placeholderShort;
-    const pathname = usePathname()
-    const searchParams = useSearchParams()
-    const setModalOpen = () => {
-        setOpen(true)
-    }
-    useEffect(() => {
-        setOpen(false)
-    }, [pathname, searchParams]);
-    useEffect(() => { 
-        modalState(open)
-    }, [open]);
-    useEffect(() => {
-      if (open) {setOpen(false)}
-    },[isDesktop,isLgScreen,isTablet,isPhone])
-    return (
-        <Collapsible.Root
-        className={`CollapsibleRoot font-medium mr-12 h-full hover:text-brand-green transition-colors`}
-        open={open} 
-        onOpenChange={setModalOpen}
+  }, [isDesktop, isLgScreen, isTablet, isPhone])
+  return (
+    <Collapsible.Root
+      className={`CollapsibleRoot font-medium mr-12 h-full hover:text-brand-green transition-colors`}
+      open={open}
+      onOpenChange={setModalOpen}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
       >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Collapsible.Trigger asChild>
-            <Link href={''} onClick={setModalOpen} className={`${ !isDesktop ? 'text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide' : 'mr-11 text-l font-medium text-grey-600 hover:text-brand-green transition-colors'}`}>
-              {!isDesktop ? homeContent?.search.menuTitle : homeContent?.search.menuTitle.toUpperCase()}
-            </Link>
-          </Collapsible.Trigger>
-        </div>
-        <div className={`h-screen w-screen fixed display: ${open ? 'inline': 'hidden'} ${open && isDesktop ? ' bg-white/95 top-0 left-0' : ''}  ${open && !isDesktop ? ' bg-white top-[-245px] left-0' : ''} ${open && !isDesktop && window.innerWidth < 769 ? ' bg-white top-[-265px] left-0' : ''} z-80 ${open && 'transition ease-in-out delay-50 duration-150'}`}>
-        <OutsideClickHandler onOutsideClick={() => closeAll()}>
-          <Collapsible.Content
-            className="collapseContent text-grey-600"
+        <Collapsible.Trigger asChild>
+          <Link
+            href={""}
+            onClick={setModalOpen}
+            className={`${
+              !isDesktop
+                ? "text-white text-xl md:text-2xl lg:text-4xl font-medium tracking-wide"
+                : "mr-11 text-l font-medium text-grey-600 hover:text-brand-green transition-colors"
+            }`}
           >
-            <div id="modal" ref={ref} className={`${!isDesktop ? 'absolute' : 'fixed'} top-[50%] left-0 h-full w-full flex justify-center align-middle ${open && 'transition ease-in-out delay-150 duration-300'}`}>
-              <div id='search-container' style={{ transform: `translateY(${translateY}px)` }} className={` ${isDesktop ? 'w-[849px]' : 'w-[343px]'} transition-transform`}>
-                {searchContent && <Search data={searchContent} placeholder={searchPlaceholder} searchBoxActive={searchBoxActive} setSearchBoxActive={setSearchBoxActive} />}
+            {!isDesktop
+              ? homeContent?.search.menuTitle
+              : homeContent?.search.menuTitle.toUpperCase()}
+          </Link>
+        </Collapsible.Trigger>
+      </div>
+      <div
+        className={`h-screen w-screen fixed display: ${
+          open ? "inline" : "hidden"
+        } ${open && isDesktop ? " bg-white/95 top-0 left-0" : ""}  ${
+          open && !isDesktop ? " bg-white top-[-245px] left-0" : ""
+        } ${
+          open && !isDesktop && window.innerWidth < 769
+            ? " bg-white top-[-265px] left-0"
+            : ""
+        } z-80 ${open && "transition ease-in-out delay-50 duration-150"}`}
+      >
+        <OutsideClickHandler onOutsideClick={() => closeAll()}>
+          <Collapsible.Content className="collapseContent text-grey-600">
+            <div
+              id="modal"
+              ref={ref}
+              className={`${
+                !isDesktop ? "absolute" : "fixed"
+              } top-[50%] left-0 h-full w-full flex justify-center align-middle ${
+                open && "transition ease-in-out delay-150 duration-300"
+              }`}
+            >
+              <div
+                id="search-container"
+                style={{ transform: `translateY(${translateY}px)` }}
+                className={` ${
+                  isDesktop ? "w-[849px]" : "w-[343px]"
+                } transition-transform`}
+              >
+                {searchContent && (
+                  <Search
+                    data={searchContent}
+                    placeholder={searchPlaceholder}
+                    searchBoxActive={searchBoxActive}
+                    setSearchBoxActive={setSearchBoxActive}
+                  />
+                )}
               </div>
             </div>
           </Collapsible.Content>
         </OutsideClickHandler>
-        </div>
-      </Collapsible.Root>
-    )
+      </div>
+    </Collapsible.Root>
+  )
 }
 
 export default SearchModal
